6e1b8180eccb12df431d63f93ff4ef36ce50145c
RFuentes2 <fuentesr682@gmail.com>
2024-05-21 11:54:03 -0500

    sliders (verificar)

--- js/slider.js
+++ js/slider.js
@@ -16,14 +16,19 @@
 
     
     const changePosition = (add)=>{
-        const currentTestimony = document.querySelector('testimony_body--show').dataset.id;
+        const currentTestimony = document.querySelector('.testimony_body--show').dataset.id;
         value = Number(currentTestimony);
         value += add;
 
+
+        sliders[Number(currentTestimony)-1].classList.remove('testimony_body--show');
         if(value === sliders.length+1 || value === 0){
             value = value === 0 ? sliders.length  :  1;
 
         }
+
+        sliders[value-1].classList.add('testimony_body--show');
+
     }
 
     
